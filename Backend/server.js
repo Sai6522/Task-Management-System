523aacd0b802daa0d04e72296491534a58473bd3
Venkata Sai Prasad Pulaparthi <104031565+Sai6522@users.noreply.github.com>
2024-02-11 18:47:53 +0530

    Add files via upload

---
+++ Backend/server.js
@@ -0,0 +1,64 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const Employee = require("./models/Employee")
+
+const app = express();
+app.use(express.json());
+
+     mongoose.connect(
+    process.env.MONGO_URI ,
+    {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+    }
+);
+
+
+app.get("/", (req, res) => {
+  res.send("Hey There , Greetings From The Server. Have a Good Day :)")
+    
+})
+app.get("/employee", (req, res) => {
+    
+  Employee.find({  })
+  .then((data) => {
+      res.json(data);
+  })
+  .catch((error) => {
+      console.log('error: ', error);
+  });
+  });
+  
+  
+app.get("/task", (req, res) => {
+    res.json({ message: "This is the Tasks page" });
+  });
+  
+  app.post("/task", (req, res) => {
+    const { fullName,
+        email,
+        department,
+        city,
+        tasktitle,
+        taskdeadline, } = req.body;
+
+    let newEmployee = new Employee({ 
+        fullName,
+        email,
+        department,
+        city,
+        tasktitle,
+        taskdeadline,
+    })
+    newEmployee.save();
+    console.log("Employees details and tasks has been saved")
+    
+  });
+  
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log("serve at http://localhost:5000"));
---
+++ frontend/src/redux/store.js
@@ -0,0 +1,37 @@
+//////// All the states data is stored ///////////
+// step 1 --> Redux
+
+import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+import rootReducer from "./rootReducer";
+
+function saveToLocalStorage(state) {
+    try {
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem('state', serializedState)
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+
+function loadFromLocalStorage(state) {
+    try {
+        const serializedState = localStorage.getItem("state");
+        if (serializedState === null) return undefined 
+        return JSON.parse(serializedState)
+     
+    } catch (e) {
+        console.log(e)
+        return undefined
+    }
+}
+
+const persistedState = loadFromLocalStorage()
+const store = createStore(rootReducer, persistedState,composeWithDevTools());
+// const store = createStore(rootReducer, composeWithDevTools());
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
+
+export default store;
